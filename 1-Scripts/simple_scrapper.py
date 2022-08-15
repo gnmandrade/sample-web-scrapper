50ac3023b28908bb6e0af1309a22d40263483363
gandrade <90654001+gnmandrade@users.noreply.github.com>
2022-08-15 19:22:30 +0100

    Finished simple web scrapper. Currently working for a single search page of the sapo casas website.

--- 1-Scripts/simple_scrapper.py
+++ 1-Scripts/simple_scrapper.py
@@ -11,3 +11,60 @@ It is an adaptation, considering a different website.
 @author: gnmandrade
 """
 
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+import pandas as pd
+
+#####
+# Download the data
+#####
+
+# Define the page to scrappe
+url_to_scrappe = 'https://casa.sapo.pt/comprar-apartamentos/lisboa/'
+
+# Create the communication interface
+request_page = urlopen(url_to_scrappe)
+
+# Get the data in the page
+page_html = request_page.read()
+
+# Close the connection
+request_page.close()
+
+
+
+#####
+# Open chanel to store data
+#####
+
+filename = '2-Outputs/store_products.csv'
+f = open(filename, 'w')
+
+header = "property_type;property_location;price\n"
+
+f.write(header)
+
+#####
+# Process the data
+#####
+
+# Create BeautifulSoup object
+html_soup = BeautifulSoup(page_html, 'html.parser')
+
+# Get list of items
+store_items = html_soup.find_all('div', class_="property-info-content")
+
+# Iterate lists of items to get name and price
+for store_item in store_items:
+    property_type = store_item.find('div', class_ = "property-type").text
+    property_location = store_item.find('div', class_ = "property-location").text
+    price = store_item.find('div', class_ = "property-price-value").text
+    
+    f.write(property_type + ';' + property_location + ';' + price + '\n')
+
+#####
+# Close chanel to store data
+#####
+
+f.close()
